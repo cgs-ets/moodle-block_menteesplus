e796ac6474b76c74e828b5a2468e1a969e25b4ab
Michael de Raadt <michaelderaadt@gmail.com>
2019-11-08 09:52:16 +1100

    Initial commit

---
+++ README.txt
@@ -0,0 +1,16 @@
+The Mentees Plus block shows parents/mentors information about the students they are responsible for.
+
+To Install
+1. Copy the completion_progress directory to the blocks/ directory of your Moodle instance
+2. Visit the notifications page
+
+For more information, visit...
+http://docs.moodle.org/en/Installing_contributed_modules_or_plugins
+
+Once the Completion Progress block is installed, you can use it in a course as follows.
+
+1. Turn editing on
+4. Add the Mentees Plus block to your page
+5. Move your block into a prominent position
+6. (Optional) Configure the block with a title like "Your students"
+
---
+++ block_menteesplus.php
@@ -0,0 +1,164 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Mentees Plus block.
+ *
+ * @package    block_menteesplus
+ * @copyright  2019 Michael de Raadt michaelderaadt@gmai.com
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+class block_menteesplus extends block_base {
+
+    /**
+     * Sets the block title
+     *
+     * @return void
+     */
+    public function init() {
+        $this->title = get_string('pluginname', 'block_menteesplus');
+    }
+
+    /**
+     * Defines where the block can be added
+     *
+     * @return array
+     */
+    public function applicable_formats() {
+        return array(
+            'course-view'    => true,
+            'site'           => true,
+            'mod'            => false,
+            'my'             => false
+        );
+    }
+
+    /**
+     * Controls the block title based on instance configuration
+     *
+     * @return bool
+     */
+    public function specialization() {
+        if (isset($this->config->title) && trim($this->config->title) != '') {
+            $this->title = format_string($this->config->title);
+        } else {
+            $this->title = get_string('newmenteesblock', 'block_menteesplus');
+        }
+    }
+
+    /**
+     * Controls whether multiple instances of the block are allowed on a page
+     *
+     * @return bool
+     */
+    public function instance_allow_multiple() {
+        return true;
+    }
+
+    /**
+     * Creates the blocks main content
+     *
+     * @return string
+     */
+    public function get_content() {
+        global $CFG, $USER, $DB, $OUTPUT;
+
+        if ($this->content !== null) {
+            return $this->content;
+        }
+
+        $this->content = new stdClass();
+
+        // Get mentees for the current user.
+        $userfields = user_picture::fields('u');
+        $sql = "SELECT u.id, $userfields
+                  FROM {role_assignments} ra, {context} c, {user} u
+                 WHERE ra.userid = :mentorid
+                   AND ra.contextid = c.id
+                   AND c.instanceid = u.id
+                   AND c.contextlevel = :contextlevel";
+        $params = ['mentorid' => $USER->id, 'contextlevel' => CONTEXT_USER];
+        if ($users = $DB->get_records_sql($sql, $params)) {
+
+            // Display list of mentees.
+            $this->content->text = html_writer::start_tag('div', ['class' => 'menteesplus']);
+            foreach ($users as $user) {
+
+                // Output the mentee's photo.
+                $this->content->text .= html_writer::start_tag('div', ['class' => 'mentee']);
+                $this->content->text .= $OUTPUT->user_picture($user);
+
+                // Output the mentee's name.
+                $namelink = html_writer::link($CFG->wwwroot.'/user/view.php?id='.$user->id.'&course='.SITEID, fullname($user));
+                $this->content->text .= $namelink;
+                $this->content->text .= ' ';
+
+                // Get the mentee's courses.
+                $usercourses = enrol_get_users_courses($user->id, true, array('enddate'), 'sortorder');
+                $currentcourses = array_filter($usercourses, function($course) {
+                    $classify = course_classify_for_timeline($course);
+                    return $classify == COURSE_TIMELINE_INPROGRESS;
+                });
+
+                // Output the mentee's courses.
+                foreach ($currentcourses as $key => $course) {
+                    $coursename = $CFG->navshowfullcoursenames ? $course->fullname : $course->shortname;
+                    $courselink = html_writer::link($CFG->wwwroot.'/course/view.php?id='.$course->id, $coursename);
+                    $this->content->text .= html_writer::start_tag('span', ['class' => 'menteecourse btn btn-primary']);
+                    $this->content->text .= $courselink;
+                    $this->content->text .= html_writer::end_tag('span');
+                    $this->content->text .= ' ';
+                }
+                $this->content->text .= html_writer::end_tag('div');
+            }
+            $this->content->text .= html_writer::end_tag('div');
+        }
+
+        $this->content->footer = '';
+
+        return $this->content;
+    }
+
+    /**
+     * Returns true if the block can be docked.
+     * The menteesplus block can only be docked if it has a non-empty title.
+     *
+     * @return bool
+     */
+    public function instance_can_be_docked() {
+        return parent::instance_can_be_docked() && isset($this->config->title) && !empty($this->config->title);
+    }
+
+    /**
+     * Return the plugin config settings for external functions.
+     *
+     * @return stdClass the configs for both the block instance and plugin
+     * @since Moodle 3.8
+     */
+    public function get_config_for_external() {
+        // Return all settings for all users since it is safe (no private keys, etc..).
+        $configs = !empty($this->config) ? $this->config : new stdClass();
+
+        return (object) [
+            'instance' => $configs,
+            'plugin' => new stdClass(),
+        ];
+    }
+}
+
